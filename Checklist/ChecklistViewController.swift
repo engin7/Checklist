a0fb268843bfee352ad332f49759b17336aa8e54
Engin KUK <kuk.engin@gmail.com>
2020-01-22 20:57:47 +0300

    first cell

---
+++ Checklist/ChecklistViewController.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  Checklist
+//
+//  Created by Engin KUK on 22.01.2020.
+//  Copyright © 2020 Silverback Inc. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        return cell
+    }
+    
+    
+}
+
